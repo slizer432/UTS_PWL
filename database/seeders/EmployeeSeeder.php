1808a66b45bc031fecaad85da6f9bfd78a86fb9c
Rayhan Heru Arifiansyah <rayhanheruk@gmail.com>
2025-04-21 11:09:16 +0700

    tambah seeder

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -15,22 +15,52 @@ class EmployeeSeeder extends Seeder
     {
             DB::table('employees')->insert([
             [
-            'name' => 'Arif',
-            'salary' => 5000000,
-            'registered_since' =>  '2020-06-30',
-            'job_position' => 'Front-end Developer',
+                'name' => 'Arif',
+                'salary' => 5000000,
+                'registered_since' => '2020-06-30',
+                'job_position' => 'Front-end Developer',
             ],
             [
-            'name' => 'Joko',
-            'salary' => 4000000,
-            'registered_since' =>  '2021-02-20',
-            'job_position' => 'Back-end Developer',
+                'name' => 'Joko',
+                'salary' => 4000000,
+                'registered_since' => '2021-02-20',
+                'job_position' => 'Back-end Developer',
             ],
             [
-            'name' => 'Ali',
-            'salary' => 7000000,
-            'registered_since' =>  '2019-04-20',
-            'job_position' => 'Full-Stack Developer',
+                'name' => 'Ali',
+                'salary' => 7000000,
+                'registered_since' => '2019-04-20',
+                'job_position' => 'Full-Stack Developer',
+            ],
+            [
+                'name' => 'Alice Johnson',
+                'salary' => 5000000,
+                'registered_since' => '2022-01-15',
+                'job_position' => 'Manager',
+            ],
+            [
+                'name' => 'Bob Smith',
+                'salary' => 4200000,
+                'registered_since' => '2021-06-01',
+                'job_position' => 'Developer',
+            ],
+            [
+                'name' => 'Carla Diaz',
+                'salary' => 3800000,
+                'registered_since' => '2023-03-10',
+                'job_position' => 'Designer',
+            ],
+            [
+                'name' => 'David Lee',
+                'salary' => 4500000,
+                'registered_since' => '2020-11-23',
+                'job_position' => 'Accountant',
+            ],
+            [
+                'name' => 'Eva Green',
+                'salary' => 4000000,
+                'registered_since' => '2022-08-05',
+                'job_position' => 'HR',
             ],
         ]);
     }
